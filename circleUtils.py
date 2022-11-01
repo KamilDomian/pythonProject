e0ef196c636eeb08adb1a60919f7755182f23bf6
Kamil Domian <kamdomian@gmail.com>
2022-11-01 17:45:33 +0100

    Głupie zmiany

--- circleUtils.py
+++ circleUtils.py
@@ -6,10 +6,10 @@ from numberGetter import input_float_number
 def pole_task():
     promien = input_float_number("Podaj promień koła: ")
     pole = math.pi * promien ** 2
-    print(f"Pole koła o promieniu {promien} wynosi: {pole}")
+    print(f"Pole koła o promieniu {promien} wynosi  : {pole}")
 
 
 def obwod_task():
     promien = input_float_number("podaj promien koła: ")
     obwod = math.pi * promien * 2
-    print(f"Obwód koła o promieniu: {promien}  wynosi:  {obwod}")
+    print(f"Obwód koła o promieniu: {promien}  wynosi  :  {obwod}")
